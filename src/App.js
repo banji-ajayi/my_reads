3a847400760f31398875524502ef162b1a9ed4d1
olufemi2 <femi.banji-ajayi@adroyte.com>
2017-11-20 09:33:18 +0200

    Initial Submission to Udacity

---
+++ src/App.js
@@ -0,0 +1,127 @@
+// Site referenced
+//MDN, Javascripttutorial.net, Udacity tutors...
+
+import React from 'react'
+import * as BooksAPI from './BooksAPI'
+import Books from './Books'
+import { Link } from 'react-router-dom'
+import { Route } from 'react-router-dom'
+import './App.css'
+
+class BooksApp extends React.Component {
+
+  state = {
+    myBooks: [],
+    query: "",
+    booksReturned: [],
+    myBookShelves: [
+      {
+        bookTitle: "Currently Reading",
+        id: "currentlyReading"
+      },
+      {
+        bookTitle: "Want to Read",
+        id: "wantToRead"
+      },
+      {
+        bookTitle: "Read",
+        id: "read"
+      }
+    ]
+  }
+
+  getBooks() {
+    BooksAPI.getAll().then((books) => {
+      this.setState({ myBooks:books })
+    })
+  }
+
+
+  componentDidMount() {
+    this.getBooks()
+  }
+
+  changeBookState = (event, book) => {
+    BooksAPI.update(book, event.target.value).then(() => {
+      this.getBooks()
+    })
+  }
+
+
+  queryBooks(query) {
+    BooksAPI.search(query, 30).then((booksReturned) => {
+      booksReturned.map(gotBook =>
+        this.state.myBooks.filter(book => book.id === gotBook.id)
+      )
+      this.setState({ booksReturned })
+    })
+  }
+
+  updateQuery = (query) => {
+    this.setState({ query: query.trim() })
+    this.queryBooks(this.state.query)
+  }
+
+  render() {
+    const { booksReturned, myBooks, myBookShelves, query } = this.state
+    return (
+      <div className="app">
+
+      <Route exact path='/' render={() => (
+        <div className="list-books">
+          <div className="list-books-title">
+            <h1>My Reads</h1>
+          </div>
+          <div className="list-books-content">
+            <div>
+              {myBookShelves.map(bookShelf => (
+                <div className="bookshelf" key={bookShelf.id}>
+                  <h2 className="bookshelf-title">{bookShelf.bookTitle}</h2>
+
+                  <div className="bookshelf-books">
+                    <Books myBooks={myBooks.filter(book => book.shelf === bookShelf.id)} onChangeBookState={this.changeBookState} />
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+          <div className="open-search">
+            <Link to="/search">New book</Link>
+          </div>
+        </div>
+      )}/>
+
+
+
+        <Route path='/search' render={() => (
+          <div className="search-books">
+            <div className="search-books-bar">
+              <Link to="/"  onClick={(event) => this.updateQuery("")}>Close</Link>
+              <div>
+                <form>
+                  <input type="text" placeholder="Search books" 
+                  value={query} onChange={(event) => this.updateQuery(event.target.value)} />
+                </form>
+              </div>
+            </div>
+            <div className="search-books-results">
+              {query !== "" && (
+                <div className="bookshelf">
+                  <div className="bookshelf-books">
+                    {booksReturned.length ? (<Books myBooks={booksReturned} onChangeBookState={this.changeBookState} />) :
+                     ( <div>No results.</div>
+                    )}
+                  </div>
+                </div>
+              )}
+            </div>          
+            </div>
+        )}/>
+
+      </div>
+    )
+  }
+}
+
+export default BooksApp
+
---
+++ src/Books.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+
+Books.propTypes = {
+  myBooks: PropTypes.array.isRequired,
+  onChangeBookState: PropTypes.func.isRequired
+}
+
+function Books (props) {
+  return (
+    <ol className="books-grid">
+      {props.myBooks.map((book) => (
+        <li key={book.id} className="book">
+            <div className="book-top">
+              <div className="book-cover" style={{ width: 126, height: 193, 
+                backgroundImage: `url(http://books.google.com/books/)` }}></div>
+              <div className="book-shelf-changer">
+                <select onChange={(event) => props.onChangeBookState(event, book)}>
+                  <option value="currentlyReading">Currently Reading</option>
+                  <option value="wantToRead">Want to Read</option>
+                  <option value="read">Read</option>
+                  <option value="none">None</option>
+                </select>
+              </div>
+            </div>
+            <div className="book-title">{book.bookTitle}</div>
+              {book.authors !== undefined && (
+                book.authors.map((author) =>
+                  <div className="book-authors">{author}</div>
+                )
+              )}
+          
+        </li>
+      ))}
+    </ol>
+  )
+}
+
+
+export default Books
+
